a7be7b8e2f1e206df1f425a64c12222c8dc14d45
jisbruzzi <jose.sbru@gmail.com>
2018-11-05 17:46:40 -0300

    AGrego graficos de nuevo

--- scrap-horas/visu-horas.py
+++ scrap-horas/visu-horas.py
@@ -11,7 +11,7 @@ for issue in full_explanations:
     explanations[issue]={}
     efforts=explanations[issue]
     for effort in full_explanations[issue]:
-        if effort["date"]<'2018-10-23T00:00:00Z':
+        if effort["date"]<'2018-10-30T00:00:00Z':
             continue
         print("===========")
         print(issue)
