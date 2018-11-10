3a665fb436874cd5abf60847371591e8ac00467c
jisbruzzi <jose.sbru@gmail.com>
2018-11-10 13:21:43 -0300

    Agrego gráfico de HUs

--- scrap-horas/visu-horas.py
+++ scrap-horas/visu-horas.py
@@ -12,7 +12,7 @@ for issue in full_explanations:
     explanations[issue]={}
     efforts=explanations[issue]
     for effort in full_explanations[issue]:
-        if effort["date"]<'2018-10-30T00:00:00Z':
+        if effort["date"]<'2018-10-24T00:00:00Z':
             continue
         print("===========")
         print(issue)
@@ -159,10 +159,10 @@ def mostrar_columnas_personas():
     x=[]
     y=[]
     n=[]
-    for k in estimated.keys() & totals.keys():
+    for k in ["Camilaf","sofiamorseletto","santigandolfo","jisbruzzi"]:
         n.append(k)
-        x.append(estimated[k])
-        y.append(totals[k])
+        x.append(estimated.get(k,0))
+        y.append(totals.get(k,0))
     print(n)
     locations=np.arange(len(n))
     plt.bar(x=locations,height=x,color="#FF005588")
@@ -170,6 +170,76 @@ def mostrar_columnas_personas():
     plt.legend("estimación","realidad")
     plt.xticks(locations,n)
     plt.savefig("columnas_personas.png")
+    plt.clf()
+
+def mostrar_columnas_hus(con_otros=False):
+    archivo_no_trabajadas=open("hus_no_trabajadas.txt","w")
+    burnt={}
+    for i in estimations:
+        if total_explanations[i]==0:
+            print("NO HUBO TRABAJO SOBRE:")
+            print(i)
+            print(i,file=archivo_no_trabajadas)
+            continue
+        parte=re.search("HU([0-9]+) -",i)
+        hu="otros"
+        if parte==None:
+            if not con_otros:
+                continue
+        else:
+            hu=parte.group(1)
+        e=burnt.get(hu,0)
+        burnt[hu]=e+estimations[i]
+
+
+    totals={}
+    worked_issues=set()
+    for i in explanations:
+        parte=re.search("HU([0-9]+) -",i)
+        hu="otros"
+        if parte==None:
+            if not con_otros:
+                continue
+        else:
+            hu=parte.group(1)
+        
+        for p in explanations[i]:
+            total_hu=totals.get(hu,0)
+            totals[hu]=total_hu+explanations[i][p]/60.0
+
+
+    total_estimations={}
+    for i in estimations:
+        parte=re.search("HU([0-9]+) -",i)
+        hu="otros"
+        if parte==None:
+            if not con_otros:
+                continue
+        else:
+            hu=parte.group(1)
+        e=total_estimations.get(hu,0)
+        total_estimations[hu]=e+estimations[i]
+            
+        
+    
+    x=[]
+    y=[]
+    z=[]
+    n=[]
+    hus=sorted(list(burnt.keys() & totals.keys()))
+    for k in hus:
+        n.append(k)
+        x.append(burnt.get(k,0))
+        y.append(totals.get(k,0))
+        z.append(total_estimations.get(k,0))
+    locations=np.arange(len(n))
+    plt.bar(x=locations,height=z,color="#33338888")
+    plt.bar(x=locations,height=x,color="#FF005588")
+    plt.bar(x=locations,height=y,color="#00FF5588")
+    
+    plt.legend("estimación","realidad")
+    plt.xticks(locations,n)
+    plt.savefig("columnas_hus.png")
     plt.clf()
 
 def mostrar_burndown():
@@ -248,15 +318,19 @@ def mostrar_lista_HU():
 
 
 #mostrar_lista()
-"""
-mostrar_burndown()
-mostrar_puntos(False,False)
-mostrar_puntos(False,True)
-mostrar_puntos(True,False)
-mostrar_puntos(True,True)
-mostrar_columnas_personas()
-mostrar_columnas_repo()
-"""
-mostrar_lista_HU()
 
+#mostrar_burndown()
+#mostrar_puntos(False,False)
+#mostrar_puntos(False,True)
+#mostrar_puntos(True,False)
+#mostrar_puntos(True,True)
+#mostrar_columnas_personas()
+#mostrar_columnas_repo()
+#mostrar_lista_HU()
 
+mostrar_columnas_hus()
+
+#print("=============================================")
+#print(estimations)
+#print("=============================================")
+#print(explanations)
